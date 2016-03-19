089178d9658da4d8be5dca5e92d42d3427ad9cda
David Graham <david.malcom.graham@gmail.com>
2016-03-18 21:48:29 -0600

    Replace SelectorSet global with module import

--- rollup.config.bench.js
+++ rollup.config.bench.js
@@ -1,8 +1,12 @@
 import babel from 'rollup-plugin-babel';
+import nodeResolve from 'rollup-plugin-node-resolve';
 
 export default {
   entry: 'test/bench.js',
   dest: 'build/bench.js',
   format: 'iife',
-  plugins: [ babel() ]
+  plugins: [
+    babel(),
+    nodeResolve({jsnext:true})
+  ]
 };
--- rollup.config.test.js
+++ rollup.config.test.js
@@ -1,8 +1,12 @@
 import babel from 'rollup-plugin-babel';
+import nodeResolve from 'rollup-plugin-node-resolve';
 
 export default {
   entry: 'test/test.js',
   dest: 'build/test.js',
   format: 'iife',
-  plugins: [ babel() ]
+  plugins: [
+    babel(),
+    nodeResolve({jsnext: true})
+  ]
 };
