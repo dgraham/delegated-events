e6d9a044bae13157da567f50f56fe77043342dc1
Christophe Coevoet <stof@notk.org>
2021-10-05 00:08:51 +0200

    Fix the typescript type definition for `fire`
    Allowing any `EventTarget` matches the Flow type definitions

--- index.d.ts
+++ index.d.ts
@@ -3,4 +3,4 @@ type DelegatedEventListener = (this: Element, ev: CustomEvent & {currentTarget: 
 export function on<K extends keyof GlobalEventHandlersEventMap>(name: K, selector: string, listener: (this: GlobalEventHandlers & Element, ev: GlobalEventHandlersEventMap[K] & {currentTarget: Element}) => any, options?: EventListenerOptions): void;
 export function on(name: string, selector: string, listener: DelegatedEventListener, options?: EventListenerOptions): void;
 export function off(name: string, selector: string, listener: DelegatedEventListener, options?: EventListenerOptions): void;
-export function fire(target: Document | Element, name: string, detail?: any): boolean;
+export function fire(target: EventTarget, name: string, detail?: any): boolean;
