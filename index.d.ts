0b5a7fc0dfc107a665437ba59fcefe6ad007e2b3
Kristján Oddsson <koddsson@github.com>
2022-05-16 14:46:11 +0100

    Change return signature for delegated event listener

--- index.d.ts
+++ index.d.ts
@@ -1,4 +1,4 @@
-type DelegatedEventListener = (this: Element, ev: CustomEvent & {currentTarget: Element}) => any
+type DelegatedEventListener = (this: Element, ev: CustomEvent & {currentTarget: Element}) => void
 
 export function on<K extends keyof GlobalEventHandlersEventMap>(name: K, selector: string, listener: (this: GlobalEventHandlers & Element, ev: GlobalEventHandlersEventMap[K] & {currentTarget: Element}) => any, options?: EventListenerOptions): void;
 export function on(name: string, selector: string, listener: DelegatedEventListener, options?: EventListenerOptions): void;
